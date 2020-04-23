07b3a6d4fc870c12e8140074d8f5b0074db3e06e
LT-722 <ling_5945@126.com>
2020-04-23 22:53:51 +0800

    Postman可以请求登陆了

--- src/main/java/com/example/message/service/UserService.java
+++ src/main/java/com/example/message/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.message.service;
 
+import com.example.message.entity.UserEntity;
+
 public interface UserService {
-    String loginByUid(String uid, String givenPWD);
+    String loginByUid(UserEntity user);
 }
--- src/main/java/com/example/message/service/UserServiceImplement.java
+++ src/main/java/com/example/message/service/UserServiceImplement.java
@@ -1,9 +1,10 @@
 package com.example.message.service;
 
 import com.example.message.dao.UserDaoImplement;
+import com.example.message.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,10 +14,13 @@ public class UserServiceImplement implements UserService {
 
     @PostMapping("/login")
     @Override
-    public String loginByUid(@RequestParam String uid, @RequestParam String givenPWD) {
+    public String loginByUid(@RequestBody UserEntity user) {//可以直接接受json，但是需要调整UserEntity，换一个类来接收
+        String uid = user.getUid();
+        String givenPWD= user.getPassword();
         String result = udi.logInByUid(uid, givenPWD);
+        System.out.print(result);
         if(result.equals("success")){
-            return "success";
+            return "200";
         }
         else{
             return result;
